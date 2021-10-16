2579d5877c351188b70d60552a5e0d53b48f36bc
Davidram1802 <56789396+Davidram1802@users.noreply.github.com>
2021-10-16 17:54:21 +0200

    matriculado_provisional y completado columnas definitivas

--- Tablas/crear_tabla_matriculados.sql
+++ Tablas/crear_tabla_matriculados.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS PracABD1.matriculados(
     persona_id INTEGER NOT NULL,
     curso_id INTEGER NOT NULL,
-    matriculado VARCHAR(1) DEFAULT 0, -- poner como varchar
+    matriculado_provisional VARCHAR(1) DEFAULT 0, -- poner como varchar
     comentarios VARCHAR(500),
     UNIQUE (persona_id, curso_id)
 )
--- procesamientos/creacion_columnas_definitivas.sql
+++ procesamientos/creacion_columnas_definitivas.sql
@@ -3,3 +3,9 @@ ADD en_paro INTEGER;
 
 ALTER TABLE pracabd1.personas
 ADD canal INTEGER;
+
+ALTER TABLE pracabd1.personas
+ADD cod_postal INTEGER;
+
+ALTER TABLE pracabd1.matriulados
+ADD matriculado INTEGER;
