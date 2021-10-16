b1d71fd152a5efc5777b9b44d101a575f0ef0759
Davidram1802 <56789396+Davidram1802@users.noreply.github.com>
2021-10-16 18:32:42 +0200

    borrar columnas provisionales actualizado

--- procesamientos/borrar_columnas_provisionales.sql
+++ procesamientos/borrar_columnas_provisionales.sql
@@ -1,3 +1,9 @@
 ALTER TABLE pracabd1.personas DROP en_paro_provisional;
 
 ALTER TABLE pracabd1.personas DROP canal_provisional;
+
+ALTER TABLE pracabd1.personas DROP cod_postal_provisional;
+
+ALTER TABLE pracabd1.personas DROP fecha_provisional;
+
+ALTER TABLE pracabd1.matriculados DROP matriculado_provisional;
