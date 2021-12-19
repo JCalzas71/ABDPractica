871192eda80b3ed1325f8e181d6e69edf720b301
Calzas Bedoya <jesus.calzas.bedoya@alumnos.upm.es>
2021-12-19 11:44:38 +0100

    Scripts finales

--- Tablas/Practica 2/Transacciones/transacciones_root.sql
+++ Tablas/Practica 2/Transacciones/transacciones_root.sql
@@ -1,14 +1,22 @@
 -- Lectura
 START TRANSACTION;
 	SELECT *
-    FROM personas;
+    FROM cursos
+    WHERE curso_id = 11001 FOR UPDATE;
+    SELECT *
+    FROM cursos
+    WHERE curso_id = 11002 FOR UPDATE;
 COMMIT;
-
+SELECT * FROM cursos;
 -- Escritura
 START TRANSACTION;
-	UPDATE pracabd1.cursos SET curso_id = 11009 WHERE curso_id = 110010;
+	UPDATE pracabd1.cursos SET curso_id = 11011 WHERE curso_id = 11001;
 COMMIT;
 
 SET AUTOCOMMIT = 0;
 
 SET GLOBAL innodb_deadlock_detect = ON;
+
+update pracabd1.cursos SET curso_id = 11003 WHERE curso_id = 11023;
+
+select * from cursos;
